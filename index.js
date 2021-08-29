45bace63d3cd27a62c74312dcab0b34f82a540f6
Kristin <kristinponsonby@gmail.com>
2021-08-29 09:36:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const returnFirstTwoDrivers = function(array){
-        let newDriversArray = []
-        const driver1 = array[0]
-        driver1.push(newDriversArray)
-        return newDriversArray
-    }
+// const returnFirstTwoDrivers = function(array){
+//         let newDriversArray = []
+//         const driver1 = array[0]
+//         driver1.push(newDriversArray)
+//         return newDriversArray
+//     }
 
 
+const returnFirstTwoDrivers = function(drivers){
+
+}
