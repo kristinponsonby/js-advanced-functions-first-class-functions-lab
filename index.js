2babfbdb5d6875fc60e28ae022a4cb798d03cccf
Kristin <kristinponsonby@gmail.com>
2021-08-29 09:26:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+        let newDriversArray = []
+        const driver1 = array[0]
+        driver1.push(newDriversArray)
+        return newDriversArray
+    }
+
+
